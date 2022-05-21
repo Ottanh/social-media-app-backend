d4734f8d7e662b43203e01dd5833ddd750d0a7a9
Olli Tanhuanpää <ottanh@utu.fi>
2022-05-21 03:57:23 +0300

    add mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,80 @@
 import { ApolloServer, gql } from 'apollo-server';
 //import { v1 as uuid } from 'uuid';
-import postData from '../data/posts';
-import userData from '../data/users';
+import 'dotenv/config';
+import mongoose, { model, Schema } from 'mongoose';
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+if(!MONGODB_URI) {
+  throw new TypeError('MONGODB_URI is undefined');
+}
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message);
+  });
+
+interface UserType {
+  username: string;
+  name: string;
+  joined: string;
+  description: string;
+}
+
+const userSchema = new Schema<UserType>({
+  username: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  joined: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  }
+});
+
+interface PostType {
+  username: string;
+  user: string;
+  date: string;
+  content: string;
+  likes: number;
+}
+
+const postSchema = new Schema<PostType>({
+  username: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    required: true
+  }
+});
+
+const User = model<UserType>('User', userSchema);
+const Post = model<PostType>('Post', postSchema);
 
 
 const typeDefs = gql`
@@ -29,16 +102,16 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    allUsers: () => {
-      return userData;
+    allUsers: async () => {
+      return await User.find<UserType>({});
     },
-    findUser: (_root: undefined, args: { username: string; }) => {
-      return userData.find(user => user.username === args.username);
+    findUser: async (_root: undefined, args: { username: string; }) => {
+      return await User.findOne<UserType>({username: args.username});
     }
   },
   User: {
-    posts: (user: { username: string }) => {
-      return postData.filter(post => post.username === user.username);
+    posts: async (user: { username: string }) => {
+      return await Post.find({ username: user.username });
     }
   }
 };
