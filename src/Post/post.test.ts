3f58d8843f49d688ff2de891ff4c3586e0ad8435
Olli Tanhuanpää <ottanh@utu.fi>
2022-07-14 16:53:14 +0300

    add complete tests for findPosts query

--- src/Post/post.test.ts
+++ src/Post/post.test.ts
@@ -33,70 +33,120 @@ const testServer = new ApolloServer({
   }
 });
 
+const postID1 = new mongoose.Types.ObjectId();
 const userID = new mongoose.Types.ObjectId();
+const userID2 = new mongoose.Types.ObjectId();
 
 const initialPosts = [  
   {    
+    _id: postID1,
     content: 'test1',
     image: 'defaultUserPic.png',
     replyTo: null,
     user: {
       _id: userID,
       name: 'testUser',
-      username: 'testUSerName'
+      username: 'testUserName'
     }
   },  
   {    
     content: 'test2',
     image: null,
-    replyTo: 'originalPost',
+    replyTo: postID1,
     user: {
-      _id: userID,
-      name: 'testUser',
-      username: 'testUSerName'
+      _id: userID2,
+      name: 'testUser2',
+      username: 'testUserName2'
     }
-  },  
+  },
+  {    
+    content: 'test3',
+    image: null,
+    replyTo: null,
+    user: {
+      _id: userID2,
+      name: 'testUser2',
+      username: 'testUserName2'
+    }
+  }, 
 ];
 
 
-beforeEach(async () => {
-  await Post.deleteMany({});  
-  let postObject = new Post(initialPosts[0]); 
-  await postObject.save(); 
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  postObject = new Post(initialPosts[1]);  
-  await postObject.save();
-});
+describe('FindPosts', () => {
+  beforeEach(async () => {
+    await Post.deleteMany({});  
+    let postObject = new Post(initialPosts[0]); 
+    await postObject.save(); 
+    postObject = new Post(initialPosts[1]);  
+    await postObject.save();
+    postObject = new Post(initialPosts[2]);  
+    await postObject.save();
+  });
 
 
-
-test('Returns all posts', async () => {
   const FIND_POSTS = gql`
-    query findPosts($username: String) {
-      findPosts (username: $username) { 
-        id
-        user {
-          id
-          name
-          username
-        }
+    query findPosts($username: String, $replyTo: String, $userIds: [String]) {
+       findPosts (username: $username, replyTo: $replyTo, userIds: $userIds) { 
+         id
+         user {
+           id
+           name
+           username
+         }
         date
         content
         image
         likes
-        replyTo
-        replies
+         replyTo
+         replies
       }
     }
   `;
 
-  const result = await testServer.executeOperation({
-    query: FIND_POSTS,
+  test('Returns all posts', async () => {
+    const result = await testServer.executeOperation({
+      query: FIND_POSTS,
+    });
+
+    expect(result.errors).toBeUndefined();
+    expect(result.data?.findPosts).toHaveLength(3);
+    expect(result.data?.findPosts[0].content).toBe('test3');
+    expect(result.data?.findPosts[1].content).toBe('test2');
+    expect(result.data?.findPosts[2].content).toBe('test1');
   });
 
+  test('Returns posts from single user', async () => {
+    const result = await testServer.executeOperation({
+      query: FIND_POSTS,
+      variables: { username: 'testUserName'}
+    });
 
-  expect(result.errors).toBeUndefined();
-  expect(result.data?.findPosts).toHaveLength(2);
-  expect(result.data?.findPosts[0].content).toBe('test2');
-  expect(result.data?.findPosts[1].content).toBe('test1');
+    expect(result.errors).toBeUndefined();
+    expect(result.data?.findPosts).toHaveLength(1);
+    expect(result.data?.findPosts[0].content).toBe('test1');
+  });
+
+  test('Returns reply', async () => {
+    const result = await testServer.executeOperation({
+      query: FIND_POSTS,
+      variables: { replyTo: postID1.toString()}
+    });
+
+    expect(result.errors).toBeUndefined();
+    expect(result.data?.findPosts).toHaveLength(1);
+    expect(result.data?.findPosts[0].content).toBe('test2');
+  });
+
+  test('Returns posts from multiple users', async () => {
+    const result = await testServer.executeOperation({
+      query: FIND_POSTS,
+      variables: { userIds: [userID.toString(), userID2.toString()]}
+    });
+
+    expect(result.errors).toBeUndefined();
+    expect(result.data?.findPosts).toHaveLength(2);
+    expect(result.data?.findPosts[0].content).toBe('test3');
+    expect(result.data?.findPosts[1].content).toBe('test1');
+  });
+
 });
